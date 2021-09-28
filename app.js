4e978705c2789cedced5a1ee11816f3654d6d436
Luis Fabian Muñoz Ortiz <luiisfabian96@gmail.com>
2021-09-28 01:29:12 -0500

    crud Restfull completed

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-let usersList = [
-    { id: 1, name: "luis Fabian", email: "luiisfabian967@gmai.com" },
-    { id: 2, name: "Yefrid David", email: "YEfrid@gmail.com" },
-    { id: 3, name: "Javier Muñoz", email: "javier@34.com" },
-    { id: 4, name: "Lucia Ortiz", email: "Casa@casa.com" },
-]
+let usersList = {
+    1:{ id: 1, name: "luis Fabian", email: "luiisfabian967@gmai.com" },
+    2:{ id: 2, name: "Yefrid David", email: "YEfrid@gmail.com" },
+    3:{ id: 3, name: "Javier Muñoz", email: "javier@34.com" },
+    4:{ id: 4, name: "Lucia Ortiz", email: "Casa@casa.com" },
+}
 
 
 // var users = [];
@@ -27,43 +27,70 @@ app.post('/', function (req, res) {
     res.send("this is post Method");
 });
 
+app.put('/', function (req, res) {
+    res.send("this is put method");
+})
+
+app.delete('/', function(req, res){
+    res.send("this is delete method");
+})
+
 app.get('/users', function (req, res) {
 
-    res.send("Get all users")
+    res.send("Get all users");
+
+//   return  res.send(Object.values.values(usersList))
 });
 
 app.get('/users/:id', function (req, res) {
-    let found = usersList.find((user) => {
-        return user.id === parseInt(req.params.id);
-    });
 
-    if (found) {
-        res.send(found)
-    } else {
-        res.send("no encontradp");
+    return res.send("get one user")
+//   res.send(usersList[req.params.id]);
+    // let found = usersList.find((user) => {
+    //     return user.id === parseInt(req.params.id);
+    // });
+
+    // if (found) {
+    //     res.send(found)
+    // } else {
+    //     res.send("no encontradp");
+    // }
+});
+
+app.post('/users/', function(req, res){
+    console.log("Wur g");
+    let otherUser = {
+        id:5,
+        name: "numero 5",
+        email: "numero5@go.com"   
     }
+
+    usersList[6] = otherUser
+    return res.send(otherUser)
 });
 
 app.put('/users/:id', function (req, res) {
-    res.send("Updated ")
+    console.log("Que gonorrea");
+    res.send('Updated one user '+ req.params.id);
 })
 
-app.post('/users',  function (req, res) {
-    res.send("imprimio");
-    //  let miapp = req.bod
-    response = {
-        name: req.body.name,
-        email: req.body.email
-    };
-    console.log(response);
-    res.send(response);
+// app.post('/users', function (req, res) {
+//     res.send("imprimio");
+//     //  let miapp = req.bod
+//     response = {
+//         name: req.body.name,
+//         email: req.body.email
+//     };
+//     console.log(response);
+//     res.send(response);
 
-});
+// });
 
 
 
-app.delete('/users:id', function (req, res) {
-    res.send("delete user by id")
+app.delete('/users/:id', function (req, res) {
+    console.log("El eliminar 1");
+    res.send("delete user  id" + req.params.id);
 });
 
 app.delete('/users', function (req, res) {
